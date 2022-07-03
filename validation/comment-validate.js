7cbfda215b123486c2d9b2f19256b51dab222894
LoanThai <thloanth.dut@gmail.com>
2022-07-04 01:08:00 +0700

    modify validate comment news

--- validation/comment-validate.js
+++ validation/comment-validate.js
@@ -1,7 +1,7 @@
 const handleError = require('../general/Error');
 const commentSchema = require('./schema/comment-schema')
 
-const newCommentValidate = async (res, req, next) => {
+const newCommentValidate = async (req, res, next) => {
     try {
         const value = await commentSchema.validateAsync(req.body);
         next()
@@ -9,7 +9,7 @@ const newCommentValidate = async (res, req, next) => {
     catch (err) {
         const data = {
             code: 400,
-            message: err.message, 
+            message: err.message,
             res: 0
         }
         return handleError(res, data)
